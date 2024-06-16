3582f45223dee6465bc9c4674b7d242bac8ecfa4
minsoo-kim <crg2ch@gmail.com>
2024-06-16 14:38:41 +0900

    fix dag_id

--- dags/dags_python_operator.py
+++ dags/dags_python_operator.py
@@ -5,7 +5,7 @@ import random
 from airflow.operators.python import PythonOperator
 
 with DAG(
-    dag_id="dags/dags_python_operator",
+    dag_id="dags_python_operator",
     schedule="30 6 * * *",
     start_date=pendulum.datetime(2024, 6, 1, tz="Asia/Seoul"),
     catchup=False,
