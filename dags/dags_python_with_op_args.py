94f7aeb436cc55784d766e4b3f49bf2bd3345a01
minsoo-kim <crg2ch@gmail.com>
2024-06-16 15:26:37 +0900

    rename dags_id

--- dags/dags_python_with_op_args.py
+++ dags/dags_python_with_op_args.py
@@ -4,7 +4,7 @@ from airflow.operators.python import PythonOperator
 from common.common_func import regist
 
 with DAG(
-    dag_id="dags_python_with_no_args",
+    dag_id="dags_python_with_op_args",
     schedule="30 6 * * *",
     start_date=pendulum.datetime(2024, 6, 1, tz="Asia/Seoul"),
     catchup=False,
