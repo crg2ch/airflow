ac732d62ea7b6aaa47f4ae1d29b90f3f3c4f757e
minsoo-kim <crg2ch@gmail.com>
2024-06-17 03:15:21 +0900

    add files

---
+++ dags/dags_python_with_base_branch_operator.py
@@ -0,0 +1,45 @@
+from airflow import DAG
+from datetime import datetime
+from airflow.operators.python import PythonOperator
+from airflow.operators.branch import BaseBranchOperator
+
+with DAG(
+    dag_id="dags_python_with_base_branch_operator",
+    schedule=None,
+    start_date=datetime(2024,6,1),
+    catchup=False,
+) as dag:
+    class CustomBranchOperator(BaseBranchOperator):
+        def choose_branch(self, context):
+            import random
+            print(context)
+            item_lst = ['A', 'B', 'C']
+            selected_item = random.choice(item_lst)
+            if selected_item == 'A':
+                return 'task_a'
+            elif selected_item in ['B', 'C']:
+                return ['task_b', 'task_c']
+
+    def common_func(**kwargs):
+        print(kwargs['selected'])
+    
+    task_a = PythonOperator(
+        task_id='task_a',
+        python_callable=common_func,
+        op_kwargs={'selected':'A'}
+    )
+
+    task_b = PythonOperator(
+        task_id='task_b',
+        python_callable=common_func,
+        op_kwargs={'selected':'B'}
+    )
+
+    task_c = PythonOperator(
+        task_id='task_c',
+        python_callable=common_func,
+        op_kwargs={'selected':'C'}
+    )
+    
+    custom_branch_operator = CustomBranchOperator(task_id='python_branch_task')
+    custom_branch_operator >> [task_a, task_b, task_c]
