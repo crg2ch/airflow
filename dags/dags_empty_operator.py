5f71b44369dcb70543708443331bb5fb9c8eb46b
minsoo-kim <crg2ch@gmail.com>
2024-06-16 13:24:36 +0900

    dags_empty_poerator의 dag_id 수정

--- dags/dags_empty_operator.py
+++ dags/dags_empty_operator.py
@@ -4,7 +4,7 @@ import pendulum
 from airflow.operators.bash import EmptyOperator
 
 with DAG(
-    dag_id="dags_bash_operator",
+    dag_id="dags_empty_operator",
     schedule="0 0 * * *",
     start_date=pendulum.datetime(2024, 6, 1, tz="Asia/Seoul"),
     catchup=False,
