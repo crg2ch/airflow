1dd607df4023348660c248d3230e81f884f6e1d1
minsoo-kim <crg2ch@gmail.com>
2024-06-16 14:50:19 +0900

    change dag name

--- dags/dags_python_import_func.py
+++ dags/dags_python_import_func.py
@@ -1,23 +1,19 @@
 from airflow import DAG
 import datetime
 import pendulum
-import random
 from airflow.operators.python import PythonOperator
+from common.common_func import get_sftp
 
 with DAG(
-    dag_id="dags_python_operator",
+    dag_id="dags_python_import_func.py.py",
     schedule="30 6 * * *",
     start_date=pendulum.datetime(2024, 6, 1, tz="Asia/Seoul"),
     catchup=False,
 ) as dag:
-    def select_fruit():
-        fruit = ['APPLE', 'BANANA', 'ORANGE', 'AVOCADO']
-        rand_int = random.randint(0, 3)
-        print(fruit[rand_int])
     
-    py_t1 = PythonOperator(
-        task_id = 'py_t1',
-        python_callable=select_fruit
+    task_get_sftp = PythonOperator(
+        task_id = 'task_get_sftp',
+        python_callable=get_sftp
     )
 
-    py_t1
+    task_get_sftp
