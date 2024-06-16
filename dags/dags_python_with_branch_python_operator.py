5a93ad23b0bd74513333208f05e7c4f9d444fe43
minsoo-kim <crg2ch@gmail.com>
2024-06-17 02:56:57 +0900

    change schedule=None

--- dags/dags_python_with_branch_python_operator.py
+++ dags/dags_python_with_branch_python_operator.py
@@ -5,7 +5,7 @@ from airflow.operators.python import BranchPythonOperator
 
 with DAG(
     dag_id="dags_python_with_branch_python_operator",
-    schedule="None",
+    schedule=None,
     start_date=datetime(2024,6,1),
     catchup=False,
 ) as dag:
